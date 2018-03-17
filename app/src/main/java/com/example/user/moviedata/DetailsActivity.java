dad45f96b8dd679b3d173b18ce7e04685852c3aa
ksw4015 <zealot4321@naver.com>
2018-03-17 17:12:09 +0900

    Details모델 클래스와 DetailsActivity 레이아웃 작성

--- app/src/main/java/com/example/user/moviedata/DetailsActivity.java
+++ app/src/main/java/com/example/user/moviedata/DetailsActivity.java
@@ -3,6 +3,8 @@ package com.example.user.moviedata;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by USER on 2018-03-17.
@@ -10,9 +12,28 @@ import android.support.v7.app.AppCompatActivity;
 
 public class DetailsActivity extends AppCompatActivity {
 
+    ImageView movieBackdrop;
+    ImageView moviePoster;
+
+    TextView movieTitle, movieAverage, movieCount;
+    TextView movieOriginal, movieRuntime, movieGenres, movieOverview;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        movieBackdrop = (ImageView)findViewById(R.id.movie_backdrop);
+        moviePoster = (ImageView)findViewById(R.id.movie_details_poster);
+
+        movieTitle = (TextView)findViewById(R.id.movie_details_title);
+        movieAverage = (TextView)findViewById(R.id.movie_details_average);
+        movieCount = (TextView)findViewById(R.id.movie_count);
+
+        movieOriginal = (TextView)findViewById(R.id.movie_original_title);
+        movieRuntime = (TextView)findViewById(R.id.movie_runtime);
+        movieGenres = (TextView)findViewById(R.id.movie_genres);
+        movieOverview = (TextView)findViewById(R.id.movie_overview);
+
     }
 }
--- app/src/main/java/com/example/user/moviedata/model/Details.java
+++ app/src/main/java/com/example/user/moviedata/model/Details.java
@@ -1,11 +1,77 @@
 package com.example.user.moviedata.model;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by USER on 2018-03-17.
  */
 
 public class Details {
 
+    @SerializedName("title")
+    private String movieTitle;
+    @SerializedName("runtime")
+    private String runTime;
+    @SerializedName("original_title")
+    private String originalTitle;
+    @SerializedName("overview")
+    private String overView;
+    @SerializedName("poster_path")
+    private String posterPath;
+    @SerializedName("backdrop_path")
+    private String backdropPath;
+    @SerializedName("release_date")
+    private String releaseDate;
+    @SerializedName("vote_count")
+    private Integer rateCount;
+    @SerializedName("vote_average")
+    private Double rateAverage;
 
+    public Details(String movieTitle, String runTime, String originalTitle, String overView, String posterPath, String backdropPath, String releaseDate, Integer rateCount, Double rateAverage) {
+        this.movieTitle = movieTitle;
+        this.runTime = runTime;
+        this.originalTitle = originalTitle;
+        this.overView = overView;
+        this.posterPath = posterPath;
+        this.backdropPath = backdropPath;
+        this.releaseDate = releaseDate;
+        this.rateCount = rateCount;
+        this.rateAverage = rateAverage;
+    }
 
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public String getRunTime() {
+        return runTime;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public String getOverView() {
+        return overView;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public Integer getRateCount() {
+        return rateCount;
+    }
+
+    public Double getRateAverage() {
+        return rateAverage;
+    }
 }
